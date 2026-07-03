e5415a622e4f186ce3432fb33d51aed41bd7c4f2
Shravan Kumar <shravank242412@gmail.com>
2026-07-03 10:34:16 +0530

    practice java files

---
+++ src/com/vcubelab_dailytest/RectangleArea.java
@@ -0,0 +1,30 @@
+/*Batch 75
+Java 
+1. Write a Java program to create a class "Rectangle" with a method "calculateArea()". 
+The method should take no arguments and return no value. 
+Initialize the length and breadth inside the method, calculate the area of the rectangle, and display the result.
+*/
+
+
+package com.vcubelab_dailytest;
+
+public class RectangleArea {
+	int length;
+	int breadth;
+	
+	void calculateArea() {
+		length=10;
+        breadth=21;
+        int area=length*breadth;
+             System.out.println("Area of Rectangle = " + area);
+
+	}
+
+	public static void main(String[] args) {
+		
+		RectangleArea t=new RectangleArea();
+		t.calculateArea();
+
+	}
+
+}
