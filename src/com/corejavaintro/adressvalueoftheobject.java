26b1a2a1d2b87949dfbc4d77e49d2fa7d0b23737
Shravan Kumar <shravank242412@gmail.com>
2026-07-20 10:45:19 +0530

    add practice files of java program

--- src/com/corejavaintro/adressvalueoftheobject.java
+++ src/com/corejavaintro/adressvalueoftheobject.java
@@ -6,7 +6,7 @@ public class adressvalueoftheobject {
 		adressvalueoftheobject t=new adressvalueoftheobject();
 		System.out.println(t);//address of the object is 2b2fa4f7
 		int a=0x2b2fa4f7;
-		System.out.println("adrees value of t:"+a);
+		System.out.println("address value of t:"+a);
 		System.out.println("-----------------------------------");
 		System.out.println(t.hashCode());//its value of hexadecimal value of it,for ex which given above see it 2b2fa4f7 that value of the 724542711; 
 		
