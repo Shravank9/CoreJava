6b56e6dd9f43342ac9001b5824bd769c83939818
Shravan Kumar <shravank242412@gmail.com>
2026-08-30 19:51:11 +0530

    feat:Find the Happy Number in a given Number

---
+++ src/com/vcubelab_dailytest/Happy_Number1.java
@@ -0,0 +1,47 @@
+package com.vcubelab_dailytest;
+
+import java.util.Scanner;
+
+public class Happy_Number1 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Number:");
+		int a = sc.nextInt();
+		boolean happy = happynumber(a);
+		if (happy) {
+			System.out.println("Happy Number");
+		} else {
+			System.out.println("Not an Happy Number");
+		}
+
+		sc.close();
+
+	}
+
+	private static boolean happynumber(int a) {
+		
+		if(a<10) {
+			return a==1;
+		}
+		int sum=0;
+		while(a>0) {
+			int r=a%10;
+			sum+=Math.powExact(r, 2);
+			a/=10;
+		}
+		
+		
+		return happynumber(sum);
+	}
+
+}
+
+//1.Write a Java program to check whether a given number is a Happy Number.
+//definition - A Happy Number is a number where the repeated sum of the squares of its digits eventually becomes 1.
+//Input - 19 =>1² + 9² = 82
+//       8² + 2² = 68
+//       6² + 8² = 100
+//       1² + 0² + 0² = 1
+//Output - Happy Number
