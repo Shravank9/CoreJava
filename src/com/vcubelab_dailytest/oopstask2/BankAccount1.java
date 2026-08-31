f64cce0c2427a22f3396888d774cbe4f6456d8d5
Shravan Kumar <shravank242412@gmail.com>
2026-08-31 18:37:00 +0530

    feat: initialize BankAccount balance using constructor

---
+++ src/com/vcubelab_dailytest/oopstask2/BankAccount1.java
@@ -0,0 +1,78 @@
+package com.vcubelab_dailytest.oopstask2;
+
+public class BankAccount1 {
+
+	private String accountnumber;
+	private String holderName;
+	private double Balance;
+
+	public String getAccountnumber() {
+		return accountnumber;
+	}
+
+	public void setAccountnumber(String accountnumber) {
+		this.accountnumber = accountnumber;
+	}
+
+	public String getHolderName() {
+		return holderName;
+	}
+
+	public void setHolderName(String holderName) {
+		this.holderName = holderName;
+	}
+
+	public double getBalance() {
+		return Balance;
+	}
+
+	public void setBalance(double balance) {
+		Balance = balance;
+	}
+
+	public BankAccount1(String accountnumber, String holderName, double Balance) {
+
+		this.accountnumber = accountnumber;
+		this.holderName = holderName;
+		this.Balance = Balance;
+	}
+
+	void Withdrawal(double amount) {
+
+		if (amount >= 500) {
+
+			if (amount <= Balance) {
+				Balance = getBalance() - amount;
+				System.out.println("Withdrwal is Successfull!" + amount);
+			} else {
+				System.out.println("Insufficient amount ");
+			}
+		} else {
+			System.out.println("Withdrawl amount should be minimum  or above 500");
+		}
+
+	}
+
+	void depoist(double amount) {
+
+		if (amount >= 100) {
+			Balance = getBalance() + amount;
+			System.out.println("Depoist is Successfully");
+
+		} else {
+
+			System.out.println("Depoist amount should be atleast 100 or above");
+		}
+	}
+
+}
+
+//Today's Assignment
+//Batch 75-
+//Java-
+//1.Create a BankAccount class using Encapsulation.
+//Declare accountNumber, holderName, and balance as private.
+//Initialize the balance with ₹5000 using a constructor or directly.
+//Create a deposit(double amount) method to add money to the account.
+//Create a withdraw(double amount) method that allows withdrawal only if the balance is sufficient.
+//In main(), create an object, perform deposit and withdrawal operations, and display the final balance.
---
+++ src/com/vcubelab_dailytest/oopstask2/Main1.java
@@ -0,0 +1,59 @@
+package com.vcubelab_dailytest.oopstask2;
+
+import java.util.Scanner;
+
+public class Main1   {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the balance:");
+		double bal = sc.nextDouble();
+
+		BankAccount1 t = new BankAccount1("675327367733","Sura",bal);
+
+
+		while (true) {
+			System.out.println("1)Depoist \n2)Withdrawl \n3)Final Balance \n4)Exit");
+
+			System.out.println("Enter the Choice");
+			int choice = sc.nextInt();
+
+			switch (choice) {
+
+			case 1 -> {
+				System.out.println("Enter the amount you want to depoist it!");
+				double amount = sc.nextDouble();
+				t.depoist(amount);
+
+			}
+			case 2 -> {
+				System.out.println("Enter the amount you want to Withdrawl it!");
+				double amount = sc.nextDouble();
+				t.Withdrawal(amount);
+			}
+			case 3 -> {
+				System.out.println("The current balance is :" + t.getBalance());
+			}
+
+			case 4 -> {
+				return;
+
+			}
+			default -> System.out.println("Inavlid option");
+			}
+		}
+
+	}
+
+}
+
+//Today's Assignment
+//Batch 75-
+//Java-
+//1.Create a BankAccount class using Encapsulation.
+//Declare accountNumber, holderName, and balance as private.
+//Initialize the balance with ₹5000 using a constructor or directly.
+//Create a deposit(double amount) method to add money to the account.
+//Create a withdraw(double amount) method that allows withdrawal only if the balance is sufficient.
+//In main(), create an object, perform deposit and withdrawal operations, and display the final balance.
