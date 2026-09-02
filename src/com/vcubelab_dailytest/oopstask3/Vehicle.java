03333d10c33a3920e1f478f49b8a652fdfbd7d00
Shravan Kumar <shravank242412@gmail.com>
2026-09-02 18:17:18 +0530

    feat: add Vehicle and Car inheritance example - 2026-09-02

---
+++ src/com/vcubelab_dailytest/oopstask3/Vehicle.java
@@ -0,0 +1,22 @@
+package com.vcubelab_dailytest.oopstask3;
+
+public class Vehicle {
+	
+	void start(){
+		System.out.println("Vehicel Ready to start !");
+		
+		
+	}
+
+	
+	void drive() {
+		System.out.println("Vehicle is driving ");
+	}
+}
+
+//Today's Assignment
+//Java-
+//1.Create a Vehicle class with a start() method. 
+//Create a Car class that inherits 
+//Vehicle and has a drive() method.
+
