5b0e5c8595db53e23377a6a4c5f6157a3923cfbd
Shravan Kumar <shravank242412@gmail.com>
2026-09-09 21:15:35 +0530

    feat: implement employee attendance interface

---
+++ src/com/vcubelab_dailytest/oopstask6/Tester.java
@@ -0,0 +1,17 @@
+package com.vcubelab_dailytest.oopstask6;
+
+public class Tester implements Attendance {
+
+	@Override
+	public void markAttendance() {
+		System.out.println("Attendance from the Tester");
+
+	}
+
+}
+//Today's Assignment
+//Java-
+//1.A company records attendance for different employees.
+//Create an interface Attendance with method markAttendance().
+//Implement Developer, Tester, and Manager.
+//Display attendance messages with employee roles.
