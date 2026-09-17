98cd148a18c221a8360a3a4609829071235abc47
Shravan Kumar <shravank242412@gmail.com>
2026-09-17 23:26:00 +0530

    feat: add zero element check using while loop

---
+++ src/com/Practice/arrays/basic/CheckArrayHasZero.java
@@ -0,0 +1,67 @@
+package com.Practice.arrays.basic;
+
+import java.util.Scanner;
+
+public class CheckArrayHasZero {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Enter the size of an array:");
+
+		int size = sc.nextInt();
+
+		if (size <= 0) {
+
+			System.out.println("Array Element sShould be atleast 1");
+			sc.close();
+			return;
+
+		}
+
+		int[] arr = new int[size];
+		System.out.println("Enter the elements of an array:");
+
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = sc.nextInt();
+		}
+
+		boolean found = false;
+
+		int start = 0;
+		int end = arr.length;
+
+		while (start < end) {
+
+			if (arr[start] == 0) {
+
+				found = true;
+				break;
+
+			}
+			start++;
+		}
+
+		if (found) {
+			System.out.println("True");
+		} else {
+			System.out.println("False");
+		}
+
+		sc.close();
+
+	}
+
+}
+//Given an array, check whether the array contains at least one zero.
+//If at least one zero is present, print true.
+//If there is no zero, print false.
+//
+//Input: [2, 4, 0, 6, 8]
+//Output: true
+//
+//Input: [2, 4, 6, 8]
+//Output: false
+//
+//Required Loop: while loop
