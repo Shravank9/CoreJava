0ff9b8c1422b883cc9e809ec44894748fd335941
Shravan Kumar <shravank242412@gmail.com>
2026-09-21 18:34:31 +0530

    feat: add unchecked exception handling for online student portal

--- src/com/vcubelab_dailytest/exceptions/OnlinePortal.java
+++ src/com/vcubelab_dailytest/exceptions/OnlinePortal.java
@@ -8,91 +8,94 @@ public class OnlinePortal {
 
 		Scanner sc = new Scanner(System.in);
 
-		// Find length of a string → NullPointerException
+		try {
+			// Find length of a string → NullPointerException
 
-		while (true) {
-			try {
-				System.out.println("Enter the name:");
-				String name = sc.nextLine();
-				System.out.println(name.length());
-				break;
-			} catch (NullPointerException e) {
-				System.out.println("In catch");
-				e.getMessage();
+			while (true) {
+				try {
+					System.out.println("Enter the name:");
+					String name = sc.nextLine();
+					System.out.println(name.length());
+					break;
+				} catch (NullPointerException e) {
+					System.out.println("In catch");
+					e.getMessage();
+				}
 			}
-		}
 
-		// Take age input from user → InputMismatchException
+			// Take age input from user → InputMismatchException
 //		sc.nextLine();
-		while (true) {
-			try {
+			while (true) {
+				try {
 
-				System.out.println("Enter the age:");
-				String age = sc.nextLine();
-				Integer aa = Integer.parseInt(age);
-				System.out.println(aa);
-				break;
-			} catch (NumberFormatException e) {
-				System.out.println("In catch");
-				System.out.println(e.getMessage());
-				System.out.println(e.getCause());
+					System.out.println("Enter the age:");
+					String age = sc.nextLine();
+					Integer aa = Integer.parseInt(age);
+					System.out.println(aa);
+					break;
+				} catch (NumberFormatException e) {
+					System.out.println("In catch");
+					System.out.println(e.getMessage());
+					System.out.println(e.getCause());
 
-			} catch (Exception e1) {
-				System.out.println("in cath 2");
-				System.out.println(e1.getMessage());
-				System.out.println(e1.getCause());
+				} catch (Exception e1) {
+					System.out.println("in cath 2");
+					System.out.println(e1.getMessage());
+					System.out.println(e1.getCause());
 
+				}
 			}
-		}
 
-		// Calculate average marks → ArithmeticException
+			// Calculate average marks → ArithmeticException
 
-		while (true) {
-			try {
+			while (true) {
+				try {
 
-				System.out.println("Enter the Total marks:");
-				double total_marks = sc.nextDouble();
-				System.out.println("Enter the number of subjects:");
-				double total_subject = sc.nextDouble();
-				double avg = total_marks / total_subject;
-				System.out.println("Average marks:" + avg);
-				break;
-			} catch (NumberFormatException e) {
-				System.out.println("In catch");
-				System.out.println(e.getMessage());
-				System.out.println(e.getCause());
-			} catch (Exception e1) {
-				System.out.println("in cath 2");
-				System.out.println(e1.getMessage());
-				System.out.println(e1.getCause());
+					System.out.println("Enter the Total marks:");
+					double total_marks = sc.nextDouble();
+					System.out.println("Enter the number of subjects:");
+					double total_subject = sc.nextDouble();
+					double avg = total_marks / total_subject;
+					System.out.println("Average marks:" + avg);
+					break;
+				} catch (NumberFormatException e) {
+					System.out.println("In catch");
+					System.out.println(e.getMessage());
+					System.out.println(e.getCause());
+				} catch (Exception e1) {
+					System.out.println("in cath 2");
+					System.out.println(e1.getMessage());
+					System.out.println(e1.getCause());
 
+				}
 			}
-		}
 
-		// Access array element using index → ArrayIndexOutOfBoundsException.
-		while (true) {
-			try {
-				int[] transactionHistory = { 5000, 1000, 2000 };
+			// Access array element using index → ArrayIndexOutOfBoundsException.
+			while (true) {
+				try {
+					int[] transactionHistory = { 5000, 1000, 2000 };
 
-				System.out.println("\nEnter transaction index:");
-				int index = sc.nextInt();
+					System.out.println("\nEnter transaction index:");
+					int index = sc.nextInt();
 
-				System.out.println("Transaction: " + transactionHistory[index]);
-				break;
+					System.out.println("Transaction: " + transactionHistory[index]);
+					break;
 
-			} catch (ArrayIndexOutOfBoundsException e) {
-				System.out.println("ArrayIndexOutOfBoundsException: Invalid transaction index");
-				sc.nextLine();
+				} catch (ArrayIndexOutOfBoundsException e) {
+					System.out.println("ArrayIndexOutOfBoundsException: Invalid transaction index");
+					sc.nextLine();
 
-			} catch (Exception e1) {
-				System.out.println("in cath 2");
-				System.out.println(e1.getMessage());
-				System.out.println(e1.getCause());
-				sc.nextLine();
+				} catch (Exception e1) {
+					System.out.println("in cath 2");
+					System.out.println(e1.getMessage());
+					System.out.println(e1.getCause());
+					sc.nextLine();
 
-			} finally {
-				sc.close();
+				}
 			}
+
+		} finally {
+			sc.close();
 		}
 
 	}
